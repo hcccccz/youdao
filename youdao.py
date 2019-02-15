734d29105e413caf329088d99ce3128d870601db
hc <0903006293asd@gmail.com>
2019-02-14 20:06:28 -0800

    ac

--- youdao.py
+++ youdao.py
@@ -2,17 +2,18 @@ import requests
 import re
 import os
 
-def init(word):
+def init(word): #获取页面
     url = "https://www.youdao.com/w/"
     r = requests.get(url+word)
     page = r.text
     return page
-def loc_dic(page):
+
+def plain_exp(page): #获取直接释义直接释义
+    res = ""
     if "<div id=\"phrsListTab\" class=\"trans-wrapper clearfix\">" in page: #有词典结果
         container = re.search("<div class=\"trans-container\">.*?<ul>(.*?)</ul>.*?</div>",page,re.S).group(1)
         if 'wordGroup' in container: #中文
             word_groups = re.findall("<p.class=\"wordGroup\">.*?</p>",container,re.S)
-            res = []
             for word_group in word_groups:
                 word = re.sub("<.*?>","",word_group)
                 tar = re.search(r"\w\s\w",word)
@@ -25,31 +26,23 @@ def loc_dic(page):
                 else:
                     word = re.sub("<.*?>","",word_group)
                     word = "".join(word.split())
-
-
-                res.append(word)
-            return res
+                res += word+"\n"
         else: #英文
             word_groups = re.findall("<li>.*?</li>",container,re.S)
-            res = ["".join(re.sub("<.*?>","",word_group).split()) for word_group in word_groups]
-            return res
+            for word_group in word_groups:
+                rep = re.sub("<.*?>","",word_group)
+                rep_l = rep.split()
+                rep_s = "".join(rep_l)
+                res += rep_s+"\n"
+
+        return res
     else:
-        return ["没有结果"]
-def output(res):
-    for r in res:
-        print(r)
+        return ("没有结果")
+
+#------------------------------分割-------------------------------------------------------------------
+
+
 if __name__ == "__main__":
-    word = input("输入(!Q退出): ")
-    print("_"*8)
-    while word != "!Q":
-        if word == "!Q":
-            pass
-        elif word == "clear":
-            os.system("clear")
-        else:
-            page = init(word)
-            res = loc_dic(page)
-            output(res)
-        print("_"*8)
-        word = input("输入(!Q退出): ")
-        print("_"*8)
+    r = init("结果")
+    word = plain_exp(r)
+    print(word)
